9f8dc48296ddc30daeca36d7765cfe1b7c5148ec
pr_dagi <dagimtheteencoder@gmail.com>
2023-09-22 10:57:20 +0300

    scrapper added to a shedule

--- telegram_bot/scheduler_events/schedules.py
+++ telegram_bot/scheduler_events/schedules.py
@@ -1,46 +1,51 @@
+from time import sleep
+
+# schedule
 import schedule
-import time
+
+# project
+from telegram_bot.scraper import book_spider, category_spider
 from telegram_bot.helpers.executor import Execute
 from telegram_bot.helpers.event_storage import EVENT
 
 
 # ? event handler
-
-@EVENT.on('new_book_released')
+@EVENT.on("new_book_released")
 def on_new_book_released():
     # ? post_book_channel
-    Execute(
-        'controllers.post_controller',
-        'PostController@post'
-    ).exc()
+    Execute("controllers.post_controller", "PostController@post").exc()
     # TODO: send notification to user abt the new book
 
 
-@EVENT.on('book_updated')
+@EVENT.on("book_updated")
 def on_book_updated():
     # ? edit book post from the channel
-    Execute(
-        'controllers.post_controller',
-        'PostController@edit_post'
-    ).exc()
+    Execute("controllers.post_controller", "PostController@edit_post").exc()
 
 
 # emit 'new_book_released'
-schedule.every(20).seconds.do(
-    Execute(
-        'scheduler_events.book_event',
-        'BookEvent@check_new_book_release'
-    ).exc
+schedule.every(60).seconds.do(
+    Execute("scheduler_events.book_event", "BookEvent@check_new_book_release").exc()
 )
+logging.info(" check_new_book_release")
+
 
 # emit 'book_updated'
-schedule.every(15).seconds.do(
-    Execute(
-        'scheduler_events.book_event',
-        'BookEvent@check_book_update'
-    ).exc
+schedule.every(50).seconds.do(
+    Execute("scheduler_events.book_event", "BookEvent@check_book_update").exc()
 )
+
+
+def scraping_job():
+    category_spider()
+    sleep(30)
+
+    book_spider()
+
+
+schedule.every(5).minutes.do(scraping_job())
+
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    sleep(5)
