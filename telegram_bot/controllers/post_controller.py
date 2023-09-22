792db96d7fdf5bb1de96965cee86eae1f23d5492
pr_dagi <dagimtheteencoder@gmail.com>
2023-09-22 07:53:57 +0300

    add inline btn for editt post channel func

--- telegram_bot/controllers/post_controller.py
+++ telegram_bot/controllers/post_controller.py
@@ -9,14 +9,20 @@ from aiogram.utils.deep_linking import create_start_link
 # project
 from telegram_bot.bot_instance import Bot
 from telegram_bot.helpers.config import CONFIG
-    # models
+
+# models
 from telegram_bot.models.post_model import PostModel
 from telegram_bot.models.review_model import ReviewModel
 from telegram_bot.models.category_model import CategoryModel
-    # message templates
+
+# message templates
 from telegram_bot.template.telegram_channel_book_post import (
-    CHANNEL_POST_BODY,IN_STOCK_TEXT,OUT_OF_STOCK_TEXT,ORDER_NOW_BTN_TEXT,
-    ADD_TO_WISHLIST_BTN_TEXT,REVIEWS_BTN_TEXT,RATE_BTN_TEXT
+    CHANNEL_POST_BODY,
+    IN_STOCK_TEXT,
+    OUT_OF_STOCK_TEXT,
+    ORDER_NOW_BTN_TEXT,
+    ADD_TO_WISHLIST_BTN_TEXT,
+    REVIEWS_BTN_TEXT,
 )
 
 
@@ -27,22 +33,23 @@ class PostController:
     category_model = CategoryModel()
 
     # aiogram and Telegram bot config
-    CHANNEL_USERNAME = CONFIG.get("telegram_api","CHANNEL_USERNAME")\
-                    .strip('"')
+    CHANNEL_USERNAME = CONFIG.get("telegram_api", "CHANNEL_USERNAME").strip(
+        '"'
+    )
 
-    # main func 
+    # main func
     # posting books to channel
     def post(self):
-        # basic pagination algorithm 
+        # basic pagination algorithm
         # to fetch all unposted books based on limit
-        post_per_round = 50 # fetch 50 rows per query
+        post_per_round = 50  # fetch 50 rows per query
         total_book = self.post_model.unposted_books_count()
-        total_round = ceil(total_book/post_per_round)
-        
+        total_round = ceil(total_book / post_per_round)
+
         for round_number in range(total_round):
             limit_start = round_number * post_per_round
             limit_end = (round_number + 1) * post_per_round
-            fetch_limit = (limit_start,limit_end)
+            fetch_limit = (limit_start, limit_end)
 
             # fetch unposted books
             unposted_books = self.post_model.get_unposted_books(fetch_limit)
@@ -56,7 +63,8 @@ class PostController:
                 # sending post to channel
                 loop = asyncio.get_event_loop()
                 post_id = loop.run_until_complete(
-                    self.send_post_to_channel(book_code,post_data))
+                    self.send_post_to_channel(book_code, post_data)
+                )
 
                 # saving post id and changing the post status
                 self.post_model.save_post(post_id, book_code)
@@ -65,15 +73,15 @@ class PostController:
     # editing posts form the channel
     # on book attr updated
     def edit_post(self):
-        post_per_round = 50 # fetch 50 rows per query
+        post_per_round = 50  # fetch 50 rows per query
         total_book = self.post_model.updated_books_count()
-        total_round = ceil(total_book/post_per_round)
-        
+        total_round = ceil(total_book / post_per_round)
+
         for round_number in range(total_round):
             limit_start = round_number * post_per_round
             limit_end = (round_number + 1) * post_per_round
-            fetch_limit = (limit_start,limit_end)
-            
+            fetch_limit = (limit_start, limit_end)
+
             # fetch updated books
             updated_books = self.post_model.get_updated_books(fetch_limit)
             for book in updated_books:
@@ -87,30 +95,39 @@ class PostController:
                 # for updated books
                 loop = asyncio.get_event_loop()
                 loop.run_until_complete(
-                    self.edit_post_from_channel(post_id,post_data))
+                    self.edit_post_from_channel(post_id, post_data, book_code)
+                )
 
                 # changing book content status from updated-> original
-                self.post_model.update_book_content_status(book_code)
+                self.post_model.update_book_content_status(book_code,0)
 
     # class method | aiogram.bot.send_mes..
     # send message to telegram
     async def send_post_to_channel(self, book_code, message):
         post_inline_btn = await self.post_inline_btn(book_code)
 
-        response_msg = await Bot.send_message(chat_id=self.CHANNEL_USERNAME,
-            text=message,reply_markup=post_inline_btn)
+        response_msg = await Bot.send_message(
+            chat_id=self.CHANNEL_USERNAME,
+            text=message,
+            reply_markup=post_inline_btn,
+        )
 
         post_id = response_msg.message_id
 
-        return post_id # the posted message id
-    
+        return post_id  # the posted message id
+
         # class method
 
     # cls method | aiogram.bot.edit_...
     # edit message from telegram channel
-    async def edit_post_from_channel(self, post_id, message):
-        await Bot.edit_message_text(chat_id=self.CHANNEL_USERNAME,
-            message_id=post_id,text=message)
+    async def edit_post_from_channel(self, post_id, message, book_code):
+        inline_btn = self.post_inline_btn(book_code)
+        await Bot.edit_message_text(
+            chat_id=self.CHANNEL_USERNAME,
+            message_id=post_id,
+            text=message,
+            reply_markup=inline_btn,
+        )
 
     # class method
     # generating inline btn for the post
@@ -120,36 +137,29 @@ class PostController:
         order_now_btn_link = await create_start_link(
             bot=Bot,
             payload=f"order_now_btn&book_code={book_code}",
-            encode=True
+            encode=True,
         )
         order_now_btn = InlineKeyboardButton(
-                text=ORDER_NOW_BTN_TEXT,
-                url=order_now_btn_link
+            text=ORDER_NOW_BTN_TEXT, url=order_now_btn_link
         )
 
         add_to_wishlist_btn_link = await create_start_link(
-            bot=Bot,
-            payload=f"wishlist_btn&book_code={book_code}",
-            encode=True
+            bot=Bot, payload=f"wishlist_btn&book_code={book_code}", encode=True
         )
         add_to_wishlist_btn = InlineKeyboardButton(
-                text=ADD_TO_WISHLIST_BTN_TEXT,
-                url=add_to_wishlist_btn_link
+            text=ADD_TO_WISHLIST_BTN_TEXT, url=add_to_wishlist_btn_link
         )
 
         reviews_btn_link = await create_start_link(
-            bot=Bot,
-            payload=f"reviews_btn&book_code={book_code}",
-            encode=True
+            bot=Bot, payload=f"reviews_btn&book_code={book_code}", encode=True
         )
         reviews_btn = InlineKeyboardButton(
-                text=REVIEWS_BTN_TEXT,
-                url=reviews_btn_link
+            text=REVIEWS_BTN_TEXT, url=reviews_btn_link
         )
 
-        inline_btn_builder.add(order_now_btn,add_to_wishlist_btn)
+        inline_btn_builder.add(order_now_btn, add_to_wishlist_btn)
         inline_btn_builder.add(reviews_btn)
-        inline_btn_builder.adjust(2) # making 2X2 grid btn layout
+        inline_btn_builder.adjust(2)  # making 2X2 grid btn layout
 
         return inline_btn_builder.as_markup()
 
@@ -157,7 +167,8 @@ class PostController:
     def post_template(self, data):
         # category name
         category_name = self.category_model.get_book_category(
-                        data["book_category"])
+            data["book_category"]
+        )
 
         # book stock status logic
         book_stock_status = data["book_stoke_status"]
@@ -168,33 +179,34 @@ class PostController:
 
         # book rating
         book_total_rating = self.review_model.book_avg_rating(
-                            data["book_code"])
-        book_total_rating = book_total_rating if book_total_rating else 0
+            data["book_code"]
+        )
 
         # generating hashtag
         category_name_parts = category_name.split(",")
         hashtag_category_names = ""
         for category_name_part in category_name_parts:
             # formatted like {#main_category #sub_category}
-            hashtag_category_names += "#"+category_name_part.strip()\
-                                        .replace(" ","_")+" "
+            hashtag_category_names += (
+                "#" + category_name_part.strip().replace(" ", "_") + " "
+            )
 
-        hashtag_book_name = data["book_name"].replace(" ","_")
-        hashtag_author_name = data["book_author"].replace(" ","_")
+        hashtag_book_name = data["book_name"].replace(" ", "_")
+        hashtag_author_name = data["book_author"].replace(" ", "_")
 
         template = CHANNEL_POST_BODY.format(
-                        book_title=data["book_name"],
-                        author_name=data["book_author"],
-                        category_name=category_name,
-                        book_language=data["book_language"],
-                        book_etb_price=data["book_etb_price"],
-                        book_usd_price=data["book_usd_price"],
-                        book_stock_status=stock_status,
-                        book_total_rating=book_total_rating,
-                        hashtag_category_names=hashtag_category_names,
-                        hashtag_book_name=hashtag_book_name,
-                        hashtag_author_name=hashtag_author_name,
-                        book_img_url=data["book_img_url"]
-                    )
+            book_title=data["book_name"],
+            author_name=data["book_author"],
+            category_name=category_name,
+            book_language=data["book_language"],
+            book_etb_price=data["book_etb_price"],
+            book_usd_price=data["book_usd_price"],
+            book_stock_status=stock_status,
+            book_total_rating=book_total_rating,
+            hashtag_category_names=hashtag_category_names,
+            hashtag_book_name=hashtag_book_name,
+            hashtag_author_name=hashtag_author_name,
+            book_img_url=data["book_img_url"],
+        )
 
         return template
